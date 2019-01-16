984ccb57e065e6dd8a1eacd1a1095042711bb7e5
Alan <35471486+curiousdoge@users.noreply.github.com>
2019-01-15 20:38:46 -0500

    Create mainheader.cpp

---
+++ mainheader.cpp
@@ -0,0 +1,15 @@
+#ifndef CHATOT_H_INCLUDED
+#define CHATOT_H_INCLUDED
+ 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+ 
+ 
+ 
+ 
+#endif // CHATOT_H_INCLUDED
